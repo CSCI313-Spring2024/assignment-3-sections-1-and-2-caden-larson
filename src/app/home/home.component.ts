1a08a19e1525c6883f6b3052924fb51d841bbdb0
caden-larson <cadenlarson7@gmail.com>
2025-03-19 22:18:50 -0500

    Finished Milestone 11

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,9 +14,20 @@ export class HomeComponent {
   // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
   housingLocationList: HousingLocation[] = [];
 
+  filteredLocationList: HousingLocation[] = [];
+
+
+
   housingService: HousingService = inject(HousingService);
   // Injecting HousingService into the Component housingService: HousingService = inject(HousingService);
     constructor(){
       //setting housingLocationlist to the data array in HousingService
   this.housingLocationList = this.housingService.getAllHousingLocations(); }
+
+  filterResults(text:string){
+    if (!text) {
+  this.filteredLocationList = this.housingLocationList;
+  return; }
+  this.filteredLocationList = this.housingLocationList.filter((housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+  ); }
   }
